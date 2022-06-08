4b81e5280eba57d1d60339818e5fd3cfdf5ba46b
obzk123 <octavio_bill@hotmail.com>
2022-06-08 12:31:53 -0300

    first commit

---
+++ PruebaApi/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Opera;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using Twilio;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010.Account;
+using System.Net.Mail;
+using System.Configuration;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace PruebaApi
+{
+    class Program
+    {
+        static Screenshot screenshot;
+
+        static void Main(string[] args)
+        {
+            var options = new ChromeOptions();
+            options.DebuggerAddress = "127.0.0.1:9222";
+            var driver = new ChromeDriver(options);
+            int contador = 0;
+            
+            do
+            {
+                if (contador >= 10)
+                {
+                    Console.Clear();
+                    contador = 0;
+                }
+
+                SacarScreenShot(driver);
+                ValidarScreenShot();
+                contador++;
+            }
+            while (true);
+        }
+
+
+        public static void SacarScreenShot(ChromeDriver driver)
+        {
+            Console.WriteLine("Redirigiendo...");
+            driver.Navigate().GoToUrl("https://onlineservices.immigration.govt.nz/WorkingHoliday/Application/Create.aspx?CountryId=13&OffShore=1&STZ=0");
+            Thread.Sleep(15000);
+            Console.WriteLine("Sacando screenshot");
+            screenshot = (driver as ITakesScreenshot).GetScreenshot();
+            screenshot.SaveAsFile("screenshot.png", ScreenshotImageFormat.Png);
+            Console.WriteLine("Listo.");
+            Console.WriteLine("------------------------------------");
+            //driver.Manage().Cookies.DeleteAllCookies();
+
+        }
+
+        public static bool ValidarScreenShot()
+        {
+            if(LeerImagen("original.png") == LeerImagen("screenshot.png"))
+            {
+                return true;
+            }
+
+            EnviarAviso();
+            screenshot.SaveAsFile("original.png", ScreenshotImageFormat.Png);
+            return false;
+        }
+
+        public static string LeerImagen(string fileName)
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                using (var stream = System.IO.File.OpenRead(fileName))
+                {
+                    return Convert.ToBase64String(md5.ComputeHash(stream));
+                }
+            }
+        }
+        public static void EnviarAviso()
+        {
+
+            Console.WriteLine("-----------------ALERTA DE CAMBIO-----------------");
+
+            String EmailOrigen = "botdecorreo@gmail.com";
+            String EmailDestino = "botdecorreo@gmail.com";
+            String Password = "erejjtonqwgbnwgt";
+
+
+            MailMessage mail = new MailMessage(EmailOrigen, EmailDestino, "Cambio la pagina", "Cambio la pagina");
+            mail.IsBodyHtml = true;
+
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Port = 587;
+            smtpClient.Credentials = new System.Net.NetworkCredential(EmailOrigen, Password);
+           
+            smtpClient.Send(mail);
+            smtpClient.Dispose();
+
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,5 @@
+Step 1- Open prompt (cmd) and go to path the chrome with the command cd example: cd C:\Program Files (x86)\Google\Chrome\Application
+
+Step 2- Insert the next code in the prompt chrome.exe --remote-debugging-port=9222 -user-data-dir="E:\selenium\ChromeDriver"
+
+Step 3- Open "MonitoreoWeb.exe"
